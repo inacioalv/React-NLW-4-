b4ce4b05127585d188c9f5093d5e77c200d5f1d4
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-02-26 11:08:36 -0300

    Notification

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState } from 'react'
+import {createContext, ReactNode, useEffect, useState } from 'react'
 import challenges from '../../challenges.json'
 
 
@@ -34,7 +34,11 @@ export function ChallengesProvider({children}:ChallengesProviderProps){
 
     const [activeChallenge,setActiveChallenge]= useState(null)
 
-    const experienceToNextLevel = Math.pow((level +1)* 4,2)
+    const experienceToNextLevel = Math.pow((level +1)* 4,2);
+
+    useEffect(()=>{
+        Notification.requestPermission();
+    },[])//Array vazio executa uma vez
 
     function levelUp(){
         setLevel(level +1);
@@ -45,6 +49,14 @@ export function ChallengesProvider({children}:ChallengesProviderProps){
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission == 'granted'){
+            new Notification('Novo desafio',{
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
         
     }
 
